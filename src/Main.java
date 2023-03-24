dbfdfdbc07985172336646ce0f67533c1e3e555f
aksanachyrmasheva <aksanachyrmasheva@gmail.com>
2023-03-24 22:15:59 +0300

    hey

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import javax.management.monitor.StringMonitor;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String... args) {
+
+        metthod();
+
+    }
+
+    static void metthod() {
+        Scanner scanner = new Scanner(System.in);
+        Random random;
+        System.out.println("coз сураныз");
+        String word = scanner.nextLine();
+        System.out.println(word.toUpperCase());
+        System.out.println(word.trim());
+
+
+
+
+
+
+    }
+}
+//      method();
+//
+//    }
+//    static void method (){
+//        String[] n = {"Диана","Адилет","Айка"};
+//
+//
+//        for (String s : n) {
+//            System.out.println("прям молодец "+s);
+//        }
+//    }
+//        //-----------------------------------------------------------------------------------------
+//8    static int varargs(int... a) {
+//        int a1 = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            a1 += a[i];
+//
+//
+//        }
+//        return a1;
+//    }
+
